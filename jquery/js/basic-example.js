7ee9ff22b7fff2a053fbb9d2935d04da22e417fe
ransomv <rvarley2001@yahoo.com>
2015-11-03 05:40:30 -0800

    jquery dir added.  Note - join.html working, but no jquery or redirect to gallery

---
+++ jquery/js/basic-example.js
@@ -0,0 +1,9 @@
+
+$(':header').addClass('headline');
+$('li:lt(3)').hide().fadeIn(1500);
+//$('li').on('click', function() {
+//  $(this).remove();
+//});
+//$('li[id!="one"]').hide().delay(5OO).fadeln(1400); 
+//$('li[id!="one"]').hide().delay(500).fadeIn(1400);
+
